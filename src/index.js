8b51976da0f4f13a4ac3120b4c52abc46e24351e
vanesadeneb <vanessa_diesver@hotmail.com>
2022-07-18 11:07:55 -0700

    Agregando evento del DOM para tomar y pasar el valor del input al codigo de validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import validator from './validator.js';
+document.getElementById("validar").addEventListener("click", function(e){
+    e.preventDefault();
+    let creditCardNumber = document.getElementById("cardNumber").value;
+    validator.isValid(creditCardNumber);
+});
 
 console.log(validator);
