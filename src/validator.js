fda7f6100795dfb5d751c7d9b5fb2d959b217ef0
vanesadeneb <vanessa_diesver@hotmail.com>
2022-07-19 08:01:35 -0700

    Agregando algoritmo de Luhn para validar en validator.isValid

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,40 @@
 const validator = {
-  // ...
-};
+  isValid: function(creditCardNumber){
+    let reverseNumber = creditCardNumber.split("").reverse().join("");
+    let validationCardNumber = [];
+    //Algoritmo Luhn
+    //Guardando los números invertidos en el array validationCardNumber
+    for(let i=0; i < creditCardNumber.length; i++){
+      validationCardNumber.push(parseInt(reverseNumber[i]));
+    }
+    //Operacion de duplicar los números pares
+    for(let j=1; j < validationCardNumber.length; j+=2 ){
+        let doubled = validationCardNumber[j]*2;
+        validationCardNumber[j] = doubled;
+        let stringDoubled = doubled.toString();
+        //console.log(stringDoubled);
+        //Sumar los números que sean mayor a un dígito
+        if(stringDoubled.length > 1){
+            let newNumber = parseInt(stringDoubled.charAt(0)) + parseInt(stringDoubled.charAt(1));
+            validationCardNumber[j] = newNumber;          
+        }
+    }
+    //Sumar todos los dígitos
+    const sum = validationCardNumber.reduce((a,b) => a + b, 0)
+    console.log(sum);
+    console.log(validationCardNumber);
+    if(sum % 10 === 0){
+      console.log("Es valida!");
+      return true;
+    }else{
+      console.log("No es valida!");
+      return false;
+    }
+    //
+    return;
+  },
+  maskify: function(){
 
+  },
+};
 export default validator;
