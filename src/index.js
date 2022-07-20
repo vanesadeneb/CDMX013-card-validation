088545c2b1ea02faab4ded5a7b471e818a6c9339
vanesadeneb <vanessa_diesver@hotmail.com>
2022-07-19 18:57:51 -0700

    A

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import validator from './validator.js';
+
 document.getElementById("validar").addEventListener("click", function(e){
     e.preventDefault();
-    let creditCardNumber = document.getElementById("cardNumber").value;
-    validator.isValid(creditCardNumber);
+
+    const creditCardNumber = document.getElementById("cardNumber").value;
+    console.log(creditCardNumber);
+    const regex = new RegExp('^\\d+$');
+
+    if(!regex.test(creditCardNumber)){
+        alert("¡Ingresa el número de tu tarjeta de cŕedito!");
+        document.getElementById("cardNumber").value = "";
+    }else{
+        validator.isValid(creditCardNumber);
+        document.getElementById("cardNumber").value = validator.maskify(creditCardNumber);
+        const buttonLimpiar = document.getElementById("limpiar");
+        const buttonValidar = document.getElementById("validar");
+        if (buttonLimpiar.style.display !== "none") {
+            buttonLimpiar.style.display = "block";
+            buttonValidar.style.display = "none";
+        } else {
+            buttonLimpiar.style.display = "none";
+            buttonValidar.style.display = "block";
+        }
+    }    
+});
+
+document.getElementById("limpiar").addEventListener("click", function(e){
+    e.preventDefault();
+    window.location.href='index.html';
 });
 
 console.log(validator);
